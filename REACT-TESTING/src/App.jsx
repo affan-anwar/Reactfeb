03ff7ae3dc6f4e7373089bd3b1e3fb70bb3abef1
mdaffananwar <mdaffananwar2025@gmail.com>
2026-06-27 13:59:02 +0530

    Add React testing project

---
+++ REACT-TESTING/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Sum from './Sum'
+
+// App.jsx
+export default function App() {
+  return (
+    <div>
+       <h1>Welcome To Testing</h1>
+       <Sum /> 
+    </div>
+  )
+}
